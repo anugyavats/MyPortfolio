16c7170ee04fdec0c639b3210ce7ce30ab824d1b
Anugya Vats <111658328+anugyavats@users.noreply.github.com>
2023-06-24 16:53:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+
+const text = "Hi! I am Anugya Vats. Welcome to my Portfolio.";
+const typingDelay = 100; // Delay between each character typing
+const erasingDelay = 50; // Delay before erasing the text
+const newTextDelay = 2000; // Delay before typing the next text
+let charIndex = 0;
+let typingTimeout;
+
+function type() {
+  if (charIndex < text.length) {
+    document.getElementById("typing-text").textContent += text.charAt(charIndex);
+    charIndex++;
+    typingTimeout = setTimeout(type, typingDelay);
+  } else {
+    clearTimeout(typingTimeout);
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    document.getElementById("typing-text").textContent = text.substring(0, charIndex - 1);
+    charIndex--;
+    typingTimeout = setTimeout(erase, erasingDelay);
+  } else {
+    clearTimeout(typingTimeout);
+    setTimeout(type, typingDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(type, newTextDelay);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navigationLinks = document.querySelectorAll(".navigation a");
+    const sections = document.querySelectorAll("section");
+
+    function changeActiveLink() {
+      const currentPosition = window.pageYOffset;
+
+      sections.forEach((section, index) => {
+        if (
+          currentPosition >= section.offsetTop - section.offsetHeight * 0.5 &&
+          currentPosition < section.offsetTop + section.offsetHeight * 0.5
+        ) {
+          navigationLinks.forEach(link => {
+            link.classList.remove("active");
+          });
+          navigationLinks[index].classList.add("active");
+        }
+      });
+    }
+
+    window.addEventListener("scroll", changeActiveLink);
+    changeActiveLink();
+  });
+
+  // JavaScript code for highlighting the active navigation item and showing sections with transitions
+document.addEventListener("DOMContentLoaded", function() {
+    const navigationLinks = document.querySelectorAll(".navigation a");
+    const sections = document.querySelectorAll("section");
+  
+    function changeActiveLink() {
+      const currentPosition = window.pageYOffset;
+  
+      sections.forEach((section, index) => {
+        if (
+          currentPosition >= section.offsetTop - section.offsetHeight * 0.5 &&
+          currentPosition < section.offsetTop + section.offsetHeight * 0.5
+        ) {
+          navigationLinks.forEach(link => {
+            link.classList.remove("active");
+          });
+          navigationLinks[index].classList.add("active");
+  
+          sections.forEach(section => {
+            section.classList.remove("visible");
+          });
+          section.classList.add("visible");
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", changeActiveLink);
+    changeActiveLink();
+  });
+  
