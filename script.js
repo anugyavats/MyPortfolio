844e54c0e5e25cca41853c7751f5ef34f94b7421
Anugya Vats <111658328+anugyavats@users.noreply.github.com>
2023-06-24 17:14:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const text = "Hi! I am Anugya Vats. Welcome to my Portfolio.";
 const typingDelay = 100; // Delay between each character typing
 const erasingDelay = 50; // Delay before erasing the text
 const newTextDelay = 2000; // Delay before typing the next text
-let charIndex = 0;
+let charIndex = text.length; // Start with the last character
 let typingTimeout;
+
+function erase() {
+  if (charIndex > 0) {
+    document.getElementById("typing-text").textContent = text.substring(0, charIndex - 1);
+    charIndex--;
+    typingTimeout = setTimeout(erase, erasingDelay);
+  } else {
+    clearTimeout(typingTimeout);
+    setTimeout(type, newTextDelay);
+  }
+}
 
 function type() {
   if (charIndex < text.length) {
@@ -17,44 +28,12 @@ function type() {
   }
 }
 
-function erase() {
-  if (charIndex > 0) {
-    document.getElementById("typing-text").textContent = text.substring(0, charIndex - 1);
-    charIndex--;
-    typingTimeout = setTimeout(erase, erasingDelay);
-  } else {
-    clearTimeout(typingTimeout);
-    setTimeout(type, typingDelay);
-  }
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(type, newTextDelay);
+  erase();
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const navigationLinks = document.querySelectorAll(".navigation a");
-    const sections = document.querySelectorAll("section");
+// Rest of your code for the active navigation link highlighting...
 
-    function changeActiveLink() {
-      const currentPosition = window.pageYOffset;
-
-      sections.forEach((section, index) => {
-        if (
-          currentPosition >= section.offsetTop - section.offsetHeight * 0.5 &&
-          currentPosition < section.offsetTop + section.offsetHeight * 0.5
-        ) {
-          navigationLinks.forEach(link => {
-            link.classList.remove("active");
-          });
-          navigationLinks[index].classList.add("active");
-        }
-      });
-    }
-
-    window.addEventListener("scroll", changeActiveLink);
-    changeActiveLink();
-  });
 
   // JavaScript code for highlighting the active navigation item and showing sections with transitions
 document.addEventListener("DOMContentLoaded", function() {
